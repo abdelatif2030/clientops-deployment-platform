7913aa59a0491190e482015cb856736e9c0e2830
Abdelatif Mohamed <abdelatif2030@gmail.com>
2026-04-11 00:21:54 +0200

    main.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "monitoring_server_public_ip" {
   description = "Public IP of the Monitoring EC2 instance"
   value       = aws_instance.monitoring_server.public_ip
 }
+
+output "private_key_pem" {
+  value     = tls_private_key.terraform.private_key_pem
+  sensitive = true
+}
