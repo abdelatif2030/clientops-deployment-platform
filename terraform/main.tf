977c3285fb8533243a490e6444408b31980c4e88
Abdelatif Mohamed <abdelatif2030@gmail.com>
2026-04-11 00:12:31 +0200

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,18 +115,3 @@ resource "aws_instance" "monitoring_server" {
   user_data = local.user_data_root_key
 }
 
-# ---------------------------
-# Outputs
-# ---------------------------
-output "app_server_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
-
-output "monitoring_server_public_ip" {
-  value = aws_instance.monitoring_server.public_ip
-}
-
-output "private_key" {
-  value     = tls_private_key.terraform.private_key_pem
-  sensitive = true
-}
